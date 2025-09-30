01e63129f8e86673bfb393bba3a55a96b0b2747e
123EFD <erchinqq06@gmail.com>
2025-09-30 12:27:27 +0800

    Implement book creation functionality with user association; update BooksContext to include user permissions and refactor Create component for form handling

--- app/(dashboard)/create.jsx
+++ app/(dashboard)/create.jsx
@@ -1,18 +1,85 @@
-import Spaces from "../../components/Spaces";
-import ThemeText from "../../components/ThemeText";
+import { StyleSheet,Text, TouchableWithoutFeedback, Keyboard } from "react-native";
+import { useBooks } from "../../hooks/useBooks";
+import { useState } from "react";
+
+//theme components
 import ThemeView from "../../components/ThemeView";
-import { StyleSheet } from "react-native";
+import ThemeText from "../../components/ThemeText";
+import ThemeButton from "../../components/ThemeButton";
+import Spaces from "../../components/Spaces";
+import ThemeTextInput from "../../components/ThemeTextInput";
+import { useRouter } from "expo-router";
 
 const Create = () => {
-    return (
-        <ThemeView style={styles.container}>
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('');
+    const [description, setDescription] = useState('');
+    const [loading, setLoading] = useState(false);
 
-            <ThemeText title={true} style={styles.heading}>
-                Time to add new books to your shelf!
-            </ThemeText>
-            <Spaces/>
+    const { createBooks } = useBooks(); //get createBooks function from context
+    const router = useRouter();
 
-        </ThemeView>
+    const handleSubmit = async () => {
+        if (!title.trim() || !author.trim() || !description.trim()) return
+
+        setLoading(true);
+        
+        await createBooks({ title, author, description }); //pass back the data
+
+        //reset the form
+        setTitle('');
+        setAuthor('');
+        setDescription('');
+
+        //redirect
+        router.replace('/books')
+
+        //reset loading state
+        setLoading(false);
+    }
+
+    return (      
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+            <ThemeView style={styles.container}>
+
+                <ThemeText title={true} style={styles.heading}>
+                    Time to add new books to your shelf!
+                </ThemeText>
+                <Spaces/>
+
+                <ThemeTextInput
+                style={styles.input}
+                placeholder="Book Title"
+                value={title}
+                onChangeText={setTitle}
+                />
+                <Spaces/>
+
+                <ThemeTextInput
+                style={styles.input}
+                placeholder="Author"
+                value={author}
+                onChangeText={setAuthor}
+                />
+                <Spaces/>
+
+                <ThemeTextInput
+                style={styles.input}
+                placeholder="Book Description"
+                value={description}
+                onChangeText={setDescription}
+                multiline={true}
+                />
+                <Spaces/>
+
+                <ThemeButton onPress={handleSubmit} disabled={loading}> 
+                    <Text style={{ color: '#ED0593'}}>
+                        {loading ? "Saving..." : "Create Book"} 
+                    </Text>
+                </ThemeButton>
+        
+            </ThemeView>
+        </TouchableWithoutFeedback>
     )
 }
 
@@ -26,7 +93,20 @@ const styles  =StyleSheet.create({
     },
     heading:{
         fontWeight:'bold',
-        fontSize: 20,
+        fontSize: 18,
         textAlign: 'center',
-    }
+    },
+    input: {
+        padding: 20,
+        borderRadius: 6,
+        alignSelf: 'stretch',
+        marginerHorizontal: 40,
+    },
+    multiline: {
+        padding: 20,
+        borderRadius: 6,
+        minHeight: 100,
+        alignSelf: 'stretch',
+        marginHorizontal: 40,
+    },
 })
--- contexts/BooksContext.jsx
+++ contexts/BooksContext.jsx
@@ -1,14 +1,17 @@
 import { createContext, useState} from "react";
 import { databases  } from "../lib/appwrite";
+import { ID, Permission, Role } from "react-native-appwrite";
+import { useUser } from "../hooks/useUser";
 
-const DATABASE_ID = "68da722d000f4209198d"
-const COLLECTION_ID = "68da6c0a0011b4fff670"
+const DATABASE_ID = "68da6c0a0011b4fff670"
+const COLLECTION_ID = "68da722d000f4209198d"
 
 export const BooksContext = createContext();
 
 //provider function to manage all the state, destructure props arg. using children 
 export function BooksProvider({ children }) {
     const [books, setBooks] = useState([]); //empty array since we're not fetching yet
+    const { user } = useUser() //to detect the user logged in
 
     //fetch all the books
     async function fetchBooks() {
@@ -28,8 +31,21 @@ export function BooksProvider({ children }) {
     }
 
     async function createBooks(data) {
+        //save new record to the collection database
+        //spread operator to get all the data from the form, add userId to link book to user
+        //data contains the properties: title, author, description... using spread operator to get all
         try {
-            
+            const newBook = await databases.createDocument(
+                DATABASE_ID, 
+                COLLECTION_ID, 
+                ID.unique(), 
+                {...data, userId: user.$id},
+                [
+                    Permission.read(Role.user(user.$id)), //only user can read
+                    Permission.update(Role.user(user.$id)),
+                    Permission.delete(Role.user(user.$id)), 
+                ]
+            ) 
         } catch (error) {
             console.error(error.message);
         }
